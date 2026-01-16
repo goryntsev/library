ce6c00cebc373dbf00cfe1774a74c6d8f472be99
goryntsev <alexander.goryntsev@gmail.com>
2026-01-16 11:42:16 +0300

    добавляю файлы, которые раньше почему-то не добавлял

---
+++ src/components/Insert.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "@site/src/css/custom.css";
+
+export default function Insert({children}) {
+    return (
+    <div className="insert">
+        {children}
+    </div>
+    );
+};
